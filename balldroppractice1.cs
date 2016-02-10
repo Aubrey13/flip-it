f8b0584dbfac48e3a14850eca5f41760e7d8e1fe
Aubrey <art_is_life23@outlook.com>
2016-02-09 17:47:44 -0700

    addition to ball drop

---
+++ balldroppractice1.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		public float balldrop = 45;
+		public float balldroplimit = 50.0;
+		public float balldropnobounce = 0.0;
+		
+		void Update ()
+		{
+			if(Input.GetKeyDown(KeyCode.Space))
+				balldroptest();
+			void ballDropTest()
+			{
+				if(balldrop > balldroplimit);
+				{
+					Debug.Log(“Ball will pop”)
+				}
+				else if(balldrop<balldropnobounce);
+				{
+					Debug.Log(“ball won’t bounce”)
+					{
+						else
+						{
+							Debug.Log(Ball will bounce)
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

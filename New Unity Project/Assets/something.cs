95bb1c4dcfc8f7a154a807ac637fdd15b2e93d6f
Aubrey <art_is_life23@outlook.com>
2016-02-09 19:31:51 -0700

    if statements

--- New Unity Project/Assets/something.cs
+++ New Unity Project/Assets/something.cs
@@ -2,14 +2,22 @@
 using System.Collections;
 
 public class something : MonoBehaviour {
+	public float ballBounce = 40.0f;
+	public float ballLimit = 50.0f;
+	public float ballsecondary =5.0f;
 
 	// Use this for initialization
 	void Start () {
+		if (ballBounce < ballsecondary){
+			Debug.Log("ball pops");
+	}
+		else if (ballBounce > ballsecondary && ballBounce <ballLimit){
+			Debug.Log("ball will bounce");
+		}
+		else {
+			Debug.Log ("ball will fly");
 	
-	}//dasdljkasdf
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+		}
 	}
 }

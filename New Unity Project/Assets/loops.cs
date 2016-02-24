bcd228cc0398456ca99a8f9e9c94dec00c73ed2f
Aubrey <art_is_life23@outlook.com>
2016-02-23 19:03:49 -0700

    controlplayerfile for midterm

---
+++ New Unity Project/Assets/loops.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loops : MonoBehaviour {
+	void Start()
+	{
+		bool ShouldContinue = false;
+
+		do { 
+			print("lala");
+
+		}while (ShouldContinue == true);
+
+
+	}
+}
---
+++ New Unity Project/Assets/playercontrol.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playercontrol : MonoBehaviour {
+
+	public float rotateSpeed;
+	public float movementSpeed;
+	private Rigidbody rigidBody;
+	// Use this for initialization
+	void Start () {
+
+			rigidBody = GetComponent<Rigidbody>();
+		}
+			                 
+				
+			  
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Rotate(new Vector3(0, -rotateSpeed, 0));
+		}
+		if(Input.GetKey (KeyCode.RightArrow))
+		{
+			transform.Rotate(new Vector3(0, rotateSpeed, 0));
+		}
+		if(Input.GetKey (KeyCode.UpArrow))
+		{
+			rigidBody.AddRelativeForce(0, 0, movementSpeed);
+		}				                
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			rigidBody.AddRelativeForce (0, 0, -movementSpeed);
+		}
+	}
+}

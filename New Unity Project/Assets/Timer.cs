56fabc26ac618c5b323b50fc349eb22d2de5bf19
Aubrey <art_is_life23@outlook.com>
2016-03-01 17:11:06 -0700

    add changes

---
+++ New Unity Project/Assets/Timer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript1 : MonoBehaviour {
+
+	public float Timer;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+
+		Time.DeltTime
+		{
+			If(Timer < 0)
+					Destroy(gameObject);
+
+	}
+}
---
+++ New Unity Project/Assets/playerhealth.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine.UI;
+using System.Collections;
+
+public class playerhealth : MonoBehaviour {
+	public text healthText;
+	private int health = 20;
+	private int damageAmount = 1;
+
+
+
+	void Start () {
+		OnCollisionEnter(collideEnemy)
+			If(collideEnemy.transform.tag == "Enemy";
+			{ 
+				health --1; healthText print; 
+			}
+	}
+	
+	void Update () {
+		public int CollideEnemy
+			
+		void OnCollisionEnter(collideEnemy){
+			If(collideEnemy.transform.tag == "Enemy");
+			{
+				print("crash Enemy")
+			}
+	}
+}

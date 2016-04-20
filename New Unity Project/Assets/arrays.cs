7bc1c74ec962e7367d6f7574007b54269435f3e8
Aubrey <art_is_life23@outlook.com>
2016-04-19 19:41:08 -0600

    added scripts to Unity. Began the Bullet Control script, added 4variables: weakD, strongD, sstrong D, and rigidBody. Also added the Addforce for bullets and bullet speed float.

--- New Unity Project/Assets/arrays.cs
+++ New Unity Project/Assets/arrays.cs
@@ -4,9 +4,15 @@ using System.Collections;
 public class arrays : MonoBehaviour {
 
 	int[] MyInt = {3, 20, 41};
+	int [] My int = new int [5];
+	
 	public GameObject [] players
 
 	void Start () {
+
+		MyInt [0] = 3;
+		MyInt [1] = 20;
+		MyInt [2] = 41;
 
 		players = gameObject.FindGameObjectWithTag ("Player");
 	

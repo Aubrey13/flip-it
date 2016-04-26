7da910b80bb016fe6abee24f8720bd5da8c92ab4
Aubrey <art_is_life23@outlook.com>
2016-04-26 17:49:20 -0600

    enumerations

--- New Unity Project/Assets/enumerations.cs
+++ New Unity Project/Assets/enumerations.cs
@@ -3,11 +3,27 @@ using System.Collections;
 
 public class enumerations : MonoBehaviour {
 
+	enum Direction {North, South, East, West}
+
+
 	// Use this for initialization
 	void Start () {
+		Direction MyDirection;
+
+		MyDirection = Direction.North;
 	
+		ReverseDirection (MyDirection);
+
+
 	}
-	
+
+	void ReverseDirection(Direction dir)
+	{
+		if (dir == Direction.North)
+			dir = Direction.South;
+
+	}
+
 	// Update is called once per frame
 	void Update () {
 	
